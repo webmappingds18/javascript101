234567115f6f050383466ff85ee1579e84a232e0
Segat <david.segat@student.uibk.ac.at>
2018-04-11 17:25:18 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+// alert("Hello World");
+
+let nachricht = "Hallo Welt";
+//alert(nachricht);
+nachricht = "neuer Text";
+//alert(nachricht);
+console.log(nachricht);
+const linkColor ="#ff0000";
+let eine_variable = 'test';
+console.log("Variable: eine_variable: ", eine_variable);
+let highScore = 75994.4;
+console.log("Highscore ist: ", highScore / 20);
+let meineNachricht = "test1";
+console.log(meineNachricht)
+meineNachricht = 'test2';
+console.log(meineNachricht);
+meineNachricht = `Ein mehr
+bla bla bla
+bla bla
+bla
+blablabla`;
+
+console.log(meineNachricht);
+
+let fullName = 'Jeffrey "The Dude" Lebowsky';
+console.log(fullName);
+
+let isOver18 = true;
+
+console.log("über 18?", isOver18);
+let age = 17
+;
+console.log("age über 18", age > 18);
